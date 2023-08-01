ea2537b6c6513def634972e54603762f1818042a
notenoughnowjy <jyp4188@gmail.com>
2023-08-01 14:40:31 +0900

    [2023.08.01] / #4.3 Named Constructor

--- dart_Practice4/4.3_Named_Constructor.dart
+++ dart_Practice4/4.3_Named_Constructor.dart
@@ -9,18 +9,35 @@ class Player {
     required this.team,
   });
 
+// named constructor
+  Player.Blue({required String name, required int age})
+      : this.name = name,
+        this.age = age,
+        this.team = 'Blue',
+        this.xp = 0;
+
   void SayHello() {
     print(
       "$name, $age, $team, $xp",
     );
   }
+
+// positional constructor
+  Player.Red(String name, int age)
+      : this.name = name,
+        this.age = age,
+        this.team = 'Red',
+        this.xp = 100;
 }
 
 void main() {
-  Player.Blue(
+  var Blueplayer = Player.Blue(
     name: "JunYeong",
     age: 23,
-    xp: 999999,
-    team: "Red",
-  ).SayHello();
+  );
+
+  Blueplayer.SayHello();
+
+  var Redplayer = Player.Red("Consider", 23);
+  Redplayer.SayHello();
 }
