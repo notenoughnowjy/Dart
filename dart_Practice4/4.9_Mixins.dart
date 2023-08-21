21ed4922bd789cf3e35a69f02125fb8c37b4bc51
notenoughnowjy <jyp4188@gmail.com>
2023-08-21 17:13:57 +0900

    [2023.08.21] / #4.9 Mixins

---
+++ dart_Practice4/4.9_Mixins.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+mixin Strong {
+  final double strengthLevel = 1509.09;
+}
+
+mixin QuickRunner {
+  void runQuick() {
+    print("runnnnnnnnn!");
+  }
+}
+
+mixin tall {
+  final double heigh = 180;
+}
+
+enum Team { red, blue }
+
+class Person {
+  final Team team;
+
+  Person({
+    required this.team,
+  });
+
+  void sayHello() {
+    print("Hello $team");
+  }
+}
+
+class Player with Strong, QuickRunner, tall {}
+
+void main() {
+  Person(team: Team.red).sayHello();
+}
